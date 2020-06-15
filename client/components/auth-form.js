750878a14462dccf14a77ef6ec3d7030921f70c2
ZhuJohnny26 <johnny.zhu26@gmail.com>
2020-06-15 12:10:38 -0400

    removed test comment

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import {auth} from '../store'
 
 /**
- * COMPONENT test test
+ * COMPONENT 
  */
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
