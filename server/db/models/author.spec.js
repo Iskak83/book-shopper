cf1fec8fe7bd03a082fceb749abc2ff1e4ce93a5
Iskak Mantyubetov <ayzik55@gmail.com>
2020-06-16 13:14:57 -0400

    Test for Book and Author models in process

--- server/db/models/author.spec.js
+++ server/db/models/author.spec.js
@@ -10,13 +10,16 @@ describe('Author model', () => {
   })
 
   describe('attributes', () => {
-    it('returns true if the password is correct', () => {
-      expect(cody.correctPassword('bones')).to.be.equal(true)
+    it('returns object', async () => {
+      let author = await Author.create({
+        name: 'Poter'
+      })
+      expect(author.name).to.be.equal('Poter')
     })
 
-    it('returns false if the password is incorrect', () => {
-      expect(cody.correctPassword('bonez')).to.be.equal(false)
-    })
+    // it('returns false if the password is incorrect', () => {
+    //   expect(cody.correctPassword('bonez')).to.be.equal(false)
+    // })
     // end describe('correctPassword')
   }) // end describe('instanceMethods')
 }) // end describe('User model')
