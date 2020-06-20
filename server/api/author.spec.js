7cc08d50283a90cf1097d3ebd94c97e634560e54
Iskak Mantyubetov <ayzik55@gmail.com>
2020-06-20 14:36:28 -0400

    bug: Blocking on author.spec.js - fixed

--- server/api/author.spec.js
+++ server/api/author.spec.js
@@ -1,8 +1,9 @@
 const {expect} = require('chai')
-const request = require('supertest')
-const agent = require('supertest')(app)
-const db = require('../db')
+
 const app = require('../index')
+const agent = require('supertest')(app)
+const request = require('supertest')
+const db = require('../db')
 const Author = require('../db/models/author')
 
 describe('Authors routes', () => {
@@ -35,7 +36,7 @@ describe('Authors routes', () => {
   describe('GET `/api/authors/:id`', () => {
     it('serves up a single Author by its `id`', async () => {
       const response = await agent.get('/api/authors/2').expect(200)
-      expect(response.body.name).to.equal('Fullstack Academy')
+      expect(response.body.name).to.equal('Rob')
     })
   })
 })
