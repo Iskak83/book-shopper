26141750a19bc04277411b1b60ae5c70bdbe3801
Iskak Mantyubetov <ayzik55@gmail.com>
2020-06-22 16:25:28 -0400

    Feature: Admin user view

---
+++ client/components/AllUsers.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {getAllUsersThunk} from '../store/users'
+import {Link} from 'react-router-dom'
+
+class AllUsers extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      // quantity:
+    }
+  }
+
+  componentDidMount() {
+    this.props.getAllUsers()
+  }
+  handleDelete(id) {
+    this.props.deleteUser(id)
+  }
+  render() {
+    const users = this.props.users
+
+    return (
+      <div className="books-container">
+        {users === undefined || !users.length ? (
+          <h3>Loading...</h3>
+        ) : (
+          users.map(user => (
+            <div className="book-container" key={user.id}>
+              <div className="book-container-left">
+                <p>User's email: {user.email}</p>
+              </div>
+              <div className="book-container-right">
+                {user.isAdmin ? (
+                  <p>This user is an Admin</p>
+                ) : (
+                  <p>This user is not an Admin</p>
+                )}
+                <button
+                  onClick={() => this.handleDelete(user.id)}
+                  type="button"
+                  className="remove_button"
+                >
+                  Remove user
+                </button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  users: state.users,
+  user: state.user
+})
+
+const mapDispatchToProps = dispatch => ({
+  getAllUsers: () => dispatch(getAllUsersThunk()),
+  deleteUser: id => dispatch(deleteUserThunk(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllUsers)
--- server/api/users.js
+++ server/api/users.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id']
+      attributes: ['id', 'email', 'isAdmin']
     })
     res.json(users)
   } catch (err) {
@@ -25,5 +25,16 @@ router.get('/:id', async (req, res, next) => {
     res.json(user)
   } catch (err) {
     next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const removedUser = await User.findByPk(req.params.id, {attributes: ['id']})
+    await removedUser.destroy()
+    res.status(204).json('Deleted User')
+  } catch (error) {
+    console.log(error)
+    next(error)
   }
 })
